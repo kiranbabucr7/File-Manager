eb425b8c4da72944fb3c97a66b6394f4d0d29cab
kiranbabucr7 <kiran.babu@ilearningengines.com>
2021-04-06 06:08:38 +0530

    added Logout in navbar

--- react-file-manager/src/components/FIleManager/Navbar.js
+++ react-file-manager/src/components/FIleManager/Navbar.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import { Nav, Navbar } from "react-bootstrap"
-import { Link } from "react-router-dom"
+import { useAuth } from "../../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
 
 export default function NavbarComponent() {
+
+  const { logout } = useAuth()
+  const history = useHistory()
+
+  const handleLogOut = () => {
+    try{
+      logout()
+      history.push("/login")
+    }catch(error){
+    }
+  }
+
   return (
-    <Navbar bg="light" expand="xxlg">
+    <Navbar bg="d-flex justify-content-between light">
       <Navbar.Brand as={Link} to="/">
         Mad Street Den
       </Navbar.Brand>
-      <Nav.Link as={Link} to="/user">
-        Profile
-      </Nav.Link>
+      <div className="d-flex">
+        <Nav.Link as={Link} to="/user">
+          Profile
+        </Nav.Link>
+        <Nav.Link onClick={handleLogOut}>
+          Logout
+        </Nav.Link>
+      </div>   
     </Navbar>
   )
 }
